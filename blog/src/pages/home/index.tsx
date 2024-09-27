2b41ae9586cee445d618da945c671e3ce1f75a56
hitobi1014 <hitobi1014@gmail.com>
2024-09-27 20:41:53 +0900

    Carousel 추가 및 css 디자인

--- blog/src/pages/home/index.tsx
+++ blog/src/pages/home/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "components/Header";
 import Footer from "components/Footer";
 import PostList from "components/PostList";
+import Carousel from "../../components/Carousel";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Carousel/>
       <PostList/>
       <Footer/>
     </>
