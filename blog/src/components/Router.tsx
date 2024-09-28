28bb78e0f5b09885da1dcbe496d091124a96e74c
hitobi1014 <hitobi1014@gmail.com>
2024-09-28 22:21:45 +0900

    feat. 로그인 페이지 추가, auth 라우트 추가

--- blog/src/components/Router.tsx
+++ blog/src/components/Router.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import {Navigate, Route, Routes} from "react-router-dom";
 import React from "react";
 import Home from "pages/home";
@@ -7,21 +8,33 @@ import PostNew from "pages/posts/new";
 import PostEdit from "pages/posts/edit";
 import ProfilePage from "pages/profile";
 import LoginPage from "pages/login";
-import SignupPage from "pages/signup";
+import SignupPage from "pages/signup"
 
 export default function Router() {
+  // firebase Auth가 인증되었으면 true로 변경해주는 로직 추가
+  const [isAuthenticated, setAuthenticated] = useState<boolean>(false);
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/posts" element={<PostsPage/>}/>
-        <Route path="/posts/:id" element={<PostDetail/>}/>
-        <Route path="/posts/new" element={<PostNew/>}/>
-        <Route path="/posts/edit/:id" element={<PostEdit/>}/>
-        <Route path="/profile" element={<ProfilePage/>}/>
-        <Route path="/login" element={<LoginPage/>}></Route>
-        <Route path="/signup" element={<SignupPage/>}></Route>
-        <Route path="*" element={<Navigate replace to="/"/>}/>
+        {isAuthenticated ? (
+          <>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/posts" element={<PostsPage/>}/>
+            <Route path="/posts/:id" element={<PostDetail/>}/>
+            <Route path="/posts/new" element={<PostNew/>}/>
+            <Route path="/posts/edit/:id" element={<PostEdit/>}/>
+            <Route path="/profile" element={<ProfilePage/>}/>
+            <Route path="/login" element={<LoginPage/>}></Route>
+            <Route path="/signup" element={<SignupPage/>}></Route>
+            <Route path="*" element={<Navigate replace to="/"/>}/>
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/signup" element={<SignupPage/>}/>
+            <Route path="*" element={<LoginPage/>}/>
+          </>
+        )}
       </Routes>
     </>
   );
