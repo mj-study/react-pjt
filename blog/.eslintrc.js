8186237cd356082549ab9d56c26c5fc2084245bf
hitobi1014 <hitobi1014@gmail.com>
2024-10-01 23:07:44 +0900

    chore. eslint, prettier 적용

---
+++ blog/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier', // 추가: Prettier 규칙을 ESLint 설정에 적용
+    'plugin:prettier/recommended', // 추가: Prettier 플러그인의 추천 설정 추가
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
