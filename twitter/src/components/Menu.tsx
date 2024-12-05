2546ab04743bd08a6003a4cf5c0d1936f9a99929
minjun <hitobi1014@gmail.com>
2024-12-05 22:49:05 +0900

    feat. auth 회원가입 및 회원가입 폼 디자인

--- twitter/src/components/Menu.tsx
+++ twitter/src/components/Menu.tsx
@@ -1,9 +1,13 @@
 import { useNavigate } from 'react-router-dom';
 import { BsHouse } from 'react-icons/bs';
 import { BiUserCircle } from 'react-icons/bi';
-import { MdLogout } from 'react-icons/md';
+import { MdLogout, MdLogin } from 'react-icons/md';
+import { useContext } from 'react';
+import AuthContext from '../context/AuthContext';
 
 export default function MenuList() {
+  const { user } = useContext(AuthContext);
+  console.log('user: ', user);
   const navigate = useNavigate();
 
   return (
@@ -17,10 +21,17 @@ export default function MenuList() {
           <BiUserCircle />
           Profile
         </button>
-        <button type="button" onClick={() => navigate('/')}>
-          <MdLogout />
-          Logout
-        </button>
+        {user === null ? (
+          <button type="button" onClick={() => navigate('/users/login')}>
+            <MdLogin />
+            Login
+          </button>
+        ) : (
+          <button type="button" onClick={() => navigate('/')}>
+            <MdLogout />
+            Logout
+          </button>
+        )}
       </div>
     </div>
   );
--- twitter/src/components/users/SignupForm.tsx
+++ twitter/src/components/users/SignupForm.tsx
@@ -117,7 +117,7 @@ export default function SignupForm() {
 
       <div className="form__block">
         <button
-          className="form__btn-submit"
+          className="form__btn--submit"
           type="submit"
           disabled={error?.length > 0}
         >
