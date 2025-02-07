59720ede947cc4b62b9d6f2ba2ac83e956dabb1e
minjun <hitobi1014@gmail.com>
2025-02-08 02:04:35 +0900

    feat. 프로필 수정

--- twitter/src/pages/profile/edit.tsx
+++ twitter/src/pages/profile/edit.tsx
@@ -1,3 +1,151 @@
+import PostHeader from '../../components/posts/Header';
+import { toast } from 'react-toastify';
+import React, { useContext, useEffect, useState } from 'react';
+import { FiImage } from 'react-icons/fi';
+import AuthContext from '../../context/AuthContext';
+
+import { v4 as uuidv4 } from 'uuid';
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadString,
+} from 'firebase/storage';
+import { updateProfile } from 'firebase/auth';
+import { storage } from 'firebaseApp';
+import { useNavigate } from 'react-router-dom';
+
 export default function ProfileEdit() {
-  return <h1>Profile Edit Page</h1>;
+  const [displayName, setDisplayName] = useState<string>('');
+  const [imageUrl, setImageUrl] = useState<string | null>();
+  const { user } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (e: any) => {
+    let key = `${user?.uid}/${uuidv4()}`;
+    const storageRef = ref(storage, key);
+    let newImageUrl = null;
+
+    e.preventDefault();
+
+    try {
+      // 기존 이미지 삭제
+      // if (user?.photoURL) {
+      //   const imageRef = ref(storage, user?.photoURL);
+      //   await deleteObject(imageRef).catch((e) => {
+      //     console.error(e);
+      //   });
+      // }
+
+      // 이미지 업로드
+      if (imageUrl) {
+        const data = await uploadString(storageRef, imageUrl, 'data_url');
+        newImageUrl = await getDownloadURL(data?.ref);
+      }
+
+      // updateProfile 호출
+      if (user) {
+        await updateProfile(user, {
+          displayName: displayName || '',
+          photoURL: newImageUrl || '',
+        })
+          .then(() => {
+            toast.success('프로필 수정을 성공했습니다.');
+            navigate('/profile');
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+      }
+    } catch (e: any) {
+      console.error(e);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+
+    setDisplayName(value);
+  };
+
+  const handleFileUpload = (e: any) => {
+    const {
+      target: { files },
+    } = e;
+
+    const file = files?.[0];
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onloadend = (e: any) => {
+      const result = e?.currentTarget?.result;
+      setImageUrl(result);
+    };
+  };
+
+  const handleDeleteImage = () => {
+    setImageUrl(null);
+  };
+
+  useEffect(() => {
+    if (user?.photoURL) {
+      setImageUrl(user?.photoURL);
+    }
+    if (user?.displayName) {
+      setDisplayName(user?.displayName);
+    }
+  }, [user?.photoURL, user?.displayName]);
+
+  return (
+    <div className="post">
+      <PostHeader />
+      <form className="post-form" onSubmit={onSubmit}>
+        <div className="post-form__profile">
+          <input
+            type="text"
+            name="displayName"
+            className="post-form__input"
+            placeholder="이름"
+            onChange={onChange}
+            value={displayName}
+          />
+          {imageUrl && (
+            <div className="post-form__attachment">
+              <img src={imageUrl} alt="attachment" width={100} height={100} />
+              <button
+                type="button"
+                onClick={handleDeleteImage}
+                className="post-form__clear-btn"
+              >
+                삭제
+              </button>
+            </div>
+          )}
+
+          <div className="post-form__submit-area">
+            <div className="post-form__image-area">
+              <label htmlFor="file-input" className="post-form__file">
+                <FiImage className="post-form__file-icon" />
+              </label>
+            </div>
+            <input
+              type="file"
+              name="file-input"
+              id="file-input"
+              accept="image/*"
+              className="hidden"
+              onChange={handleFileUpload}
+            />
+            <input
+              type="submit"
+              value="프로필 수정"
+              className="post-form__submit-btn"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
