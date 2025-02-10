05012ca107d435e2ba17841e86b0198cf20e02dd
minjun <hitobi1014@gmail.com>
2025-02-10 23:39:00 +0900

    feat. 팔로잉/팔로우 구현

--- twitter/src/components/posts/PostBox.tsx
+++ twitter/src/components/posts/PostBox.tsx
@@ -16,6 +16,7 @@ import {
 } from 'firebase/firestore';
 import { toast } from 'react-toastify';
 import { db } from '../../firebaseApp';
+import FollowingBox from '../following/FollowingBox';
 
 interface PostBoxProps {
   post: PostProps;
@@ -61,21 +62,26 @@ export default function PostBox({ post }: PostBoxProps) {
   };
   return (
     <div className="post__box" key={post.id}>
-      <Link to={`/posts/${post?.id}`}>
-        <div className="post__box-profile">
-          <div className="post__flex">
-            {post?.profileUrl ? (
-              <img
-                src={post?.profileUrl}
-                alt="profile"
-                className="post__box-profile-img"
-              />
-            ) : (
-              <FaUserCircle className="post__box-profile-icon" />
-            )}
-            <div className="post__email">{post?.email}</div>
-            <div className="post__createdAt">{post?.createdAt}</div>
+      <div className="post__box-profile">
+        <div className="post__flex">
+          {post?.profileUrl ? (
+            <img
+              src={post?.profileUrl}
+              alt="profile"
+              className="post__box-profile-img"
+            />
+          ) : (
+            <FaUserCircle className="post__box-profile-icon" />
+          )}
+          <div className="post__flex--between">
+            <div className="post__flex">
+              <div className="post__email">{post?.email}</div>
+              <div className="post__createdAt">{post?.createdAt}</div>
+            </div>
+            <FollowingBox post={post} />
           </div>
+        </div>
+        <Link to={`/posts/${post?.id}`}>
           <div className="post__box-content">{post?.content}</div>
           {post?.imageUrl && (
             <div className="post__image-div">
@@ -95,8 +101,9 @@ export default function PostBox({ post }: PostBoxProps) {
               </span>
             ))}
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
+
       <div className="post__box-footer">
         {user?.uid === post?.uid && (
           <>
